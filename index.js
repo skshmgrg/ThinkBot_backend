55c505062b6f12e5e8f200f6a176998f2e8473ff
skshmgrg <sakshamgarg3050@gmail.com>
2025-11-03 20:07:44 +0530

    fix: use environment variable for server port configuration

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT =  process.env.PORT ||5000;
 
 // Middleware
 app.use(cors());
